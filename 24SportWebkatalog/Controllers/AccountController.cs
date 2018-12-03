36e593d37f05788197025cb9b75f575341c5fb6b
Fabian Wendell <fabianwendell@hotmail.com>
2018-12-03 16:06:47 +0100

    Add project files.

---
+++ 24SportWebkatalog/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _24SportWebkatalog.Models;
+
+
+namespace _24SportWebkatalog.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using (OurdbContext db = new OurdbContext())
+            {
+                return View(db.userAccount.ToList());
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurdbContext db = new OurdbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.firstname + " " + account.lastname + " successfully registered.";
+            }
+            return View();
+        }
+
+        //Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using (OurdbContext db = new OurdbContext())
+            {
+                var usr = db.userAccount.Where(u => u.Username == user.Username && u.Password == user.Password).FirstOrDefault();
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Username"] = usr.Username.ToString();
+                    return RedirectToAction("Loggedin");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password is wrong");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Loggedin()
+        {
+            if (Session["UserID"] !=null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
+    }
+}
---
+++ 24SportWebkatalog/Models/UserAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace _24SportWebkatalog.Models
+{
+    public class UserAccount
+    {
+        [Key]
+
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="First Name is required")]
+        public string firstname { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string lastname { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email.")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage ="Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Please confirm your password")]
+        [DataType(DataType.Password)]
+        public string confirmpassword { get; set; }
+
+    }
+}
